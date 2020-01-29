b25cd167680fd2c795da262748ba11742f9fc63c
Blandford Alex <alex.blandford@hetic.net>
2020-01-29 14:24:21 +0100

    added router to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ExampleComponents from '../pages/Example-components';
 
-const App = () => (
-  <h1>Hello la team !</h1>
-)
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <main>
+          <Switch>
+            <Route exact path="/" component={ExampleComponents} />
+          </Switch>
+        </main>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
