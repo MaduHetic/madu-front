ef500c22a4164377e5c8c2e84d988bdb46336d5d
mallano <maelallano1@gmail.com>
2020-02-17 16:41:25 +0100

    Render multiple colored markers on map

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapboxGl, { Layer, Feature, Marker } from 'react-mapbox-gl';
+import { Company } from "../../core/company";
 const Map = ReactMapboxGl({
   accessToken: "pk.eyJ1IjoiZ2FtYTk3ODAiLCJhIjoiY2p2NmR3NzA4MDA1NzQzbzdpd3IzNml3NiJ9.uqGMqqnpdiBlrnzWaxMKMg"
 });
@@ -38,14 +39,63 @@ const fakeData = [
         // postalCode,
         long: 2.32,
         lat: 48.84,
+        type: "client"
+        // greenScore,
+        // logo
+    },
+    {
+        name: "Oui",
+        price: "$",
+        // description,
+        // address,
+        // city,
+        // postalCode,
+        long: 2.33,
+        lat: 48.8511628,
+        type: "client"
+        // greenScore,
+        // logo
+    },
+    {
+        name: "Non",
+        price: "$$$",
+        // description,
+        // address,
+        // city,
+        // postalCode,
+        long: 2.33,
+        lat: 48.84,
+        type: "POI"
+        // greenScore,
+        // logo
+    },
+    {
+        name: "Peut-être",
+        price: "$$",
+        // description,
+        // address,
+        // city,
+        // postalCode,
+        long: 2.32,
+        lat: 48.82,
+        type: "POI"
         // greenScore,
         // logo
     },
 ]
 
 const MapTest = () => {
+    const companies = Company.allCompanies()
+    const getCompanies = Company.getAllCompanies()
     const map = React.createRef();
     const [centerAndZoom, setCenterAndZoom] = useState({center: [2.36, 48.858], zoom: [11.8]});
+
+
+    useEffect(() => {
+        getCompanies();
+    }, [])
+
+    console.log(companies)
 
     return (
         <Map
@@ -57,9 +107,19 @@ const MapTest = () => {
             maxBounds={[[2.23, 48.8135], [2.48, 48.9029]]}
         >
             <Layer
-                type="symbol"
+                type="circle"
                 id="marker"
-                layout={{ 'icon-image': 'rocket-15' }}
+                paint={{
+                    'circle-color': [
+                        'match',
+                        ['get', 'type'],
+                        'POI',
+                        'green',
+                        'client',
+                        'blue',
+                        /* other */ 'red'
+                        ]
+                }}
             >
                 {fakeData && fakeData.map(POI => (
                     <Feature
@@ -69,7 +129,11 @@ const MapTest = () => {
                             setCenterAndZoom({center: [e.lngLat.lng, e.lngLat.lat], zoom: [14]});
                             console.log("Name: ", e.feature.properties.name)
                         }}
-                        properties={{name: POI.name}}
+                        properties={{
+                            name: POI.name,
+                            type: POI.type
+                        }}
+                        geometry={{'circle-color': "blue"}}
                     />
                 ))}
             </Layer>
