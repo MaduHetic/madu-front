58076a4ad7b067ac01841ec49f9910c6f3fd45c4
Blandford Alex <alex.blandford@hetic.net>
2020-02-20 23:08:32 +0100

    update initial values

--- src/pages/poi/poiUpdate.js
+++ src/pages/poi/poiUpdate.js
@@ -9,131 +9,124 @@ import { Formik } from 'formik';
 
 
 const PoiView = ({ history }) => {
-
     const getPoi = Poi.getPoi();
     const {id} = useParams();
-    const poi = Poi.poi();
+    const { poi } = Poi.poi();
+    const updatePoi = Poi.updatePoi();
+
+    const [searchAddressValues, setSearchAddressValues] = useState({
+        address: poi?.poi?.address,
+        city: poi?.poi?.city,
+        postalCode: poi?.poi?.postalCode,
+        long: poi?.poi?.long,
+        lat: poi?.poi?.lat,
+    });
 
     useEffect(() => {
         getPoi(id)
       }, [id]);
 
-    if (!poi.poi)  {
-        return null;
-    } else {
-        return (
-            <Wrapper>
-                <TitleDefault>
-                    <h3 className="title">{poi.poi.name}</h3>
-                </TitleDefault>
-                <Card>
-                    <Formik
-                        initialValues={{
-                        name: "hostnfly",
-                        city: 'Paris',
-                        description: "",
-                        greenScore: "123",
-                        tags: [],
-                        type: "",
-                        price: "low",
-                        typeGreenScore: [{}],
-                        }}
-                        onSubmit={
-                            values => {
-                                updatePoi(
-                                    Object.assign(values, searchAddressValues)
-                                )
-                            }
+    if (!poi) return null;
+    return (
+        <Wrapper>
+            <TitleDefault>
+                <h3 className="title">{poi.name}</h3>
+            </TitleDefault>
+            <Card>
+                <Formik
+                    initialValues={{
+                    name: poi.name,
+                    description: poi.description,
+                    greenScore: poi.greenScore,
+                    tags: poi.tags,
+                    type: poi.type,
+                    price: poi.price,
+                    logo: poi.logo,
+                    typeGreenScore: poi.typeGreenScore,
+                    dateCreate: poi.dateCreate,
+                    }}
+                    onSubmit={
+                        values => {
+                            updatePoi(
+                                Object.assign(values, searchAddressValues)
+                            )
                         }
-                    >
-                    {({
-                        values,
-                        handleChange,
-                        handleSubmit,
-                    }) => (
-                        <form onSubmit={handleSubmit}>
-                            <div className="titleContent">
-                                <h4>Informations générales</h4>
-                            </div>
-                            <div>
-                                <span>Type de fiche :</span>
-                                <p>Point d'intérêt</p>
-                            </div>
-                            <div>
-                                <span>Categorie :</span>
-                                <p>{poi.poi.type}</p>
-                            </div>
-                            <div className="tagList">
-                                <span>Liste des tags :</span>
-                                <div>
-                                    {poi.tags.map((tag, index) => (
-                                        <Tag
-                                        key={`tag__${index}`}
-                                        colorRGB={{
-                                            r: tag.r,
-                                            g: tag.g,
-                                            b: tag.b
-                                        }}
-                                        >
-                                            {tag.tag}
-                                        </Tag>
-                                    ))}
-                                </div>
-                            </div>
-                            <div>
-                                <span>Fourchette de prix :</span>
-                                <p>{poi.poi.price}</p>
-                            </div>
-                            <div>
-                                <span>Description :</span>
-                                <p>{poi.poi.description}</p>
-                            </div>
-                            <div>
-                                <div>
-                                    <span>Adresse :</span>
-                                    <p>{poi.poi.address}</p>
-                                </div>
-                                <div>
-                                    <span>Code postal :</span>
-                                    <p>{poi.poi.postalCode}</p>
-                                </div>
-                                <div>
-                                    <span>Ville :</span>
-                                    <p>{poi.poi.city}</p>
-                                </div>
-                            </div>
-                            <div>
-                                <span>Green Score :</span>
-                                <p>{poi.poi.greenScore}%</p>
-                            </div>
-                            <div>
-                                <span>Logo :</span>
-                                <div className="logo">
-                                    <img src={poi.poi.logo} alt="logo"/>
-                                </div>
-                            </div>
-                            <div>
-                                <span>Horaires :</span>
-                                <p></p>
-                            </div>
-                            <div className="groupBtn">
-                                <CustomButton
-                                    text="Enregistrer"
-                                    size="large"
-                                    textcolor={Color.white}
-                                    backgroundcolor={Color.main}
-                                    bordercolor={Color.main}
-                                    type="submit"
-                                    href={`/point-d-interet/fiche/${poi.poi.id}`}
-                                    />
-                            </div>
-                        </form>
-                    )}
-                    </Formik>
-                </Card>
-                </Wrapper>
-                )
-            }
+                    }
+                >
+                {({
+                    values,
+                    handleChange,
+                    handleSubmit,
+                }) => (
+                    <form onSubmit={handleSubmit}>
+              <div className="titleContent">
+                  <h4>Informations générales</h4>
+                  <input type="text" name="name" />
+              </div>
+              <div>
+                  <span>Type de fiche :</span>
+                  <p>Point d'intérêt</p>
+              </div>
+              <div>
+                  <span>Categorie :</span>
+                  <p>{poi.type}</p>
+              </div>
+              <div className="tagList">
+                  <span>Liste des tags :</span>
+              </div>
+              <div>
+                  <span>Fourchette de prix :</span>
+                  <p>{poi.price}</p>
+              </div>
+              <div>
+                  <span>Description :</span>
+                  <p>{poi.description}</p>
+              </div>
+              <div>
+                <div>
+                  <span>Adresse :</span>
+                  <p>{poi.address}</p>
+                </div>
+                <div>
+                  <span>Code postal :</span>
+                  <p>{poi.postalCode}</p>
+                </div>
+                <div>
+                  <span>Ville :</span>
+                  <p>{poi.city}</p>
+                </div>
+              </div>
+              <div>
+                <span>Green Score :</span>
+                <p>{poi.greenScore}%</p>
+              </div>
+              <div>
+                <span>Logo :</span>
+                <div className="logo">
+                  <img src={poi.logo} alt="logo"/>
+                </div>
+              </div>
+              <div>
+                <span>Horaires :</span>
+                <p></p>
+              </div>
+              <div className="groupBtn">
+                <CustomButton
+                  text="Enregistrer"
+                  size="large"
+                  textcolor={Color.white}
+                  backgroundcolor={Color.main}
+                  bordercolor={Color.main}
+                  type="submit"
+                  href={`/point-d-interet/fiche/${poi.id}`}
+                />
+                </div>
+            </form>
+          )}
+        </Formik>
+      </Card>
+    </Wrapper>
+  );
 }
 
 export default withRouter(PoiView);
